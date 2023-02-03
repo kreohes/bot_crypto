5c62964c2f7d2eb095c037b55864656f629c7069
Acer <kadkini@yandex.ru>
2023-02-04 01:33:07 +0500

    add buttons for bot

--- main.py
+++ main.py
@@ -14,11 +14,16 @@ dp = Dispatcher(bot)
 menu = InlineKeyboardMarkup(row_width=1)
 menu_well = InlineKeyboardButton(text='Курсы валют')
 menu.add(menu_well)
-курс
+
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await message.reply("Привет!\nНапиши мне что-нибудь!")
+    kb = [
+        [types.KeyboardButton(text='Курсы валют'), ],
+        [types.KeyboardButton(text='Информация')],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
+    await message.reply("Привет!\nНапиши мне что-нибудь!", reply_markup=keyboard)
 
 
 @dp.message_handler(commands=['info'])
@@ -29,6 +34,15 @@ async def process_help_command(message: types.Message):
 @dp.message_handler(commands=['well'])
 async def process_well_command(message: types.Message):
     await message.reply("Курсы", reply_markup=menu)
+
+
+
+@dp.message_handler(lambda message: message.text == 'Курсы валют')
+async def cmd_random(message: types.Message):
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(types.InlineKeyboardButton(text="Нажми меня", callback_data=""))
+    await message.answer("Курсы валют:", reply_markup=keyboard)
+
 
 
 if __name__ == '__main__':
