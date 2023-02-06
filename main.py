d352f2a4b502cf514c9223423fea33057f14016c
Acer <kadkini@yandex.ru>
2023-02-07 00:18:29 +0500

    add database

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+import asyncio
+from scripts.db import Database
 from aiogram.types import ReplyKeyboardRemove, \
     ReplyKeyboardMarkup, KeyboardButton, \
     InlineKeyboardMarkup, InlineKeyboardButton
@@ -10,11 +12,11 @@ TOKEN = '5995636478:AAEcb2JUzOcrbSxqXAbmeFd9bVXttfReaus'
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-# Buttons menu
-menu = InlineKeyboardMarkup(row_width=1)
-menu_well = InlineKeyboardButton(text='Курсы валют')
-menu.add(menu_well)
 
+@dp.message_handler()
+async def process_event(message: types.Message):
+    data = Database()
+    data.connection(message.from_user.id, message.text)
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
@@ -31,18 +33,11 @@ async def process_help_command(message: types.Message):
     await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
 
 
-@dp.message_handler(commands=['well'])
-async def process_well_command(message: types.Message):
-    await message.reply("Курсы", reply_markup=menu)
-
-
-
 @dp.message_handler(lambda message: message.text == 'Курсы валют')
 async def cmd_random(message: types.Message):
     keyboard = types.InlineKeyboardMarkup()
     keyboard.add(types.InlineKeyboardButton(text="Нажми меня", callback_data=""))
     await message.answer("Курсы валют:", reply_markup=keyboard)
-
 
 
 if __name__ == '__main__':
--- scripts/db.py
+++ scripts/db.py
@@ -1,1 +1,15 @@
+import asyncio
 import sqlite3
+
+conn = sqlite3.connect('db_for_bot.db')
+
+
+class Database:
+    def __init__(self):
+        self.cur = conn.cursor()
+    def connection(self, name, act):
+        self.cur.execute("INSERT INTO logs(names, action) VALUES(?, ?)", (name, act,))
+        conn.commit()
+
+    def check_currencies(self):
+        self.cur.execute("""""")
